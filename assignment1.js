76577111c9ec35e895f49b8d5d158ce44575f41c
Lasvegasman55 <ftwalton31@yahoo.com>
2024-11-08 17:37:54 -0800

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,31 @@
+// Declaring constants for placeholders
+const hero = "Sir Laughs-a-Lot";
+const sidekick = "Giggles the Squire";
+const villain = "Count Chuckles";
+const action = "tickled";
+const object = "giggleberries";
+
+// Declaring let variables for values that can change
+let location1 = "Whimsical Woods";
+let plotTwist = "a giant jellybean catapult";
+
+// Constructing the story
+let story = `Once upon a time, in the land of ${location1}, there was a brave hero named ${hero}. Together with his trusty sidekick, ${sidekick}, they set out to defeat the mischievous villain known as ${villain}.
+Armed with nothing but their wits and a sack of ${object}, they embarked on an adventure that would be sung about for ages. The ${villain}, notorious for causing uncontrollable laughter wherever he went, had to be stopped.
+As they approached the lair of ${villain}, our heroes were ${action} by ${plotTwist}. It was a sight to behold and a story to be remembered.`;
+
+// Outputting the story to the console
+console.log(story);
+
+// Changing variable values to create a different version of the story
+location1 = "Quirky Quagmire";
+plotTwist = "an army of tickle-bots";
+
+// Constructing the new story
+story = `Once upon a time, in the land of ${location1}, there was a brave hero named ${hero}. Together with his trusty sidekick, ${sidekick}, they set out to defeat the mischievous villain known as ${villain}.
+Armed with nothing but their wits and a sack of ${object}, they embarked on an adventure that would be sung about for ages. The ${villain}, notorious for causing uncontrollable laughter wherever he went, had to be stopped.
+As they approached the lair of ${villain}, our heroes were ${action} by ${plotTwist}. It was a sight to behold and a story to be remembered.`;
+
+// Outputting the new story to the console
+console.log(story);
+
